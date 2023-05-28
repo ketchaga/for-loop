18cffe9b09ea0db476f1c598f12e02717df14f89
ketch <ketchu15@stud.thga.de>
2023-05-28 17:44:40 +0200

    Added while_loop.c

--- while_loop.c
+++ while_loop.c
@@ -4,11 +4,21 @@ int main() {
     char dateipfad[] = "PP8datei";
     printf("Dateipfad: %s\n", dateipfad);
     
-    FILE* datei = fopen(dateipfad, "r");
+    FILE* datei = fopen(dateipfad, "w");
     char zeichen;
 
     while ((zeichen = fgetc(datei)) != EOF)
         printf("%c", zeichen);
+    char text[1000];
+fseek(datei, 0, SEEK_SET);
+fgets(text, sizeof(text), datei);
+printf("%s", text);
+
+char text[1000];
+fseek(datei, 0, SEEK_SET);
+fgets(text, sizeof(text), datei);
+printf("%s", text);
+
 
     fclose(datei);
 

da89d2e6bb807cb69088c025c7c9b8daee9747d2
ketch <ketchu15@stud.thga.de>
2023-05-29 13:55:51 +0200

    Added while_loop.c

--- while_loop.c
+++ while_loop.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 
 int main() {
+  
+
+    // Schritt 7: Dateipfad übergeben und anzeigen
     char dateipfad[] = "PP8datei.txt";
     printf("Dateipfad: %s\n", dateipfad);
 
+    // Schritt 9: Datei öffnen
     FILE* datei = fopen(dateipfad, "w");
     if (datei == NULL) {
         printf("Fehler beim Öffnen der Datei.\n");
         return 1;
     }
 
+    // Schritt 11: Jedes Zeichen der Datei drucken
     char zeichen;
     printf("Inhalt der Datei: ");
-    while ((zeichen = fgetc(datei)) != EOF)
+    while ((zeichen = fgetc(datei)) != EOF) {
         printf("%c", zeichen);
+    }
     printf("\n");
 
-    fseek(datei, 0, SEEK_SET);
+
+    // Schritt 15: Datei in ein Char-Array einlesen
 
     char text[1000];
-    fgets(text, sizeof(text), datei);
-    printf("Inhalt der Datei als Char-Array: %s\n", text);
+    fseek(datei, 0, SEEK_SET);
+    if (fgets(text, sizeof(text), datei) != NULL) {
+        printf("Inhalt der Datei als Char-Array: %s\n", text);
+    } else {
+        printf("Fehler beim Lesen der Datei.\n");
+    }
+
+   
 
     fclose(datei);
 
